b370466be9072a99bb6df88d62d3625c2e0719f8
steven.turner <sturner@ctic-inc.com>
2019-04-26 13:51:48 -0700

    Add wrappers for optional field clarification. Ensure dependencies are transitive and included in consuming libraries and platforms including Android.
    Using GPB3 wrapper classes to determine if fields in the report are populated when serialized. Updated test to reflect changes and API usage.

--- src/test/java/gov/agilemeridian/gnss/GnssReportProtoTest.java
+++ src/test/java/gov/agilemeridian/gnss/GnssReportProtoTest.java
@@ -1,31 +1,44 @@
 package gov.agilemeridian.gnss;
 
+import com.google.protobuf.FloatValue;
+import com.google.protobuf.FloatValueOrBuilder;
+import com.google.protobuf.Int32Value;
 import com.google.protobuf.InvalidProtocolBufferException;
 import com.google.protobuf.Timestamp;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.Base64;
 import java.util.Random;
 import java.util.stream.IntStream;
 import java.util.zip.Deflater;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class GnssReportProtoTest
 {
 
     @Test
-    public void testSerialization()
+    public void testSer2()
     {
+
+        System.out.println("TESTING!!!!");
         GnssReportProto.GnssReport.Builder report = GnssReportProto.GnssReport.newBuilder();
 
-        int lat = (int) (38.89874 * 100000.0);
-        int lon = (int) (-77.03768 * 100000.0);
+        //Legacy for determing serialization size performance. GPB does RLE on floating point.
+        int lat = (int) (38.89874 * 10000000.0);
+        int lon = (int) (-77.03768 * 10000000.0);
 
-        report.setLatLng(GnssReportProto.LatLng.newBuilder().setLatitude(lat).setLongitude(lon).build());
+        double latD = lat * 0.0000001;
+        double lonD = lon * 0.0000001;
+
+        report.setLatLng(GnssReportProto.LatLng.newBuilder().setLatitude(38.89874f).setLongitude(-77.03768f).build());
 
         report.setCallsign("TommyKnocker");
-        report.setAltitude(32000.4f);
-        long millis = System.currentTimeMillis();
+        report.setAltitude(FloatValue.newBuilder().setValue(3300f).build());
+        long millis = 3;
         Timestamp timestamp = Timestamp.newBuilder().setSeconds(millis / 1000)
                 .setNanos((int) ((millis % 1000) * 1000000)).build();
         report.setSysTime(timestamp);
@@ -35,14 +48,15 @@ public class GnssReportProtoTest
                 {
                     GnssReportProto.GNSSMeasurement.Builder measure = GnssReportProto.GNSSMeasurement.newBuilder();
 
-                    measure.setAgcdB(rand.nextFloat());
+
+                    measure.setAgcdB( FloatValue.newBuilder().setValue(3.3f).build());
                     measure.setRfStatus(GnssReportProto.RfStatus.SPOOFED);
                     //measure.setCarrierFrequencyHz(rand.nextLong());
-                    //measure.setSvId(rand.nextInt());
-                    measure.setHDop(rand.nextFloat());
+                    measure.setSvId( Int32Value.newBuilder().setValue(4).build());
+                    measure.setHDop( FloatValue.newBuilder().setValue(1.3f).build());
                     //measure.setVDop(rand.nextFloat());
                     measure.setConstellation(GnssReportProto.Constellation.GPS);
-                    //measure.setCn0DbHz(rand.nextFloat());
+                    measure.setCn0DbHz( FloatValue.newBuilder().setValue(33.9f).build());
                     //measure.setSNrdB(rand.nextFloat());
                     report.addMeasurements(measure);
                 }
@@ -51,8 +65,12 @@ public class GnssReportProtoTest
         GnssReportProto.PacketWrapper wrapperToEncode = GnssReportProto.PacketWrapper.newBuilder().setGnssReport(report).build();
 
 
+        byte[] original = wrapperToEncode.toByteArray();
+        String javaEncoded = Base64.getEncoder().encodeToString(original);
+        byte[] serialized = Base64.getDecoder().decode(javaEncoded);
 
-        byte[] serialized = wrapperToEncode.toByteArray();
+        boolean equals = Arrays.equals(original, serialized);
+
 
         try
         {
@@ -63,6 +81,9 @@ public class GnssReportProtoTest
                 case GNSSREPORT:
                     wrapperDeserialized.getGnssReport();
                     assertTrue(wrapperDeserialized.getGnssReport().hasLatLng());
+                    assertFalse(wrapperDeserialized.getGnssReport().getMeasurements(0).hasSNrdB());
+                    assertTrue(wrapperDeserialized.getGnssReport().getMeasurements(0).hasCn0DbHz());
+                    assertEquals(wrapperDeserialized.getGnssReport().getMeasurements(0).getCn0DbHz().getValue(),33.9f, 0.0);
                     break;
                 case PAYLOAD_NOT_SET:
                 default:
@@ -81,8 +102,67 @@ public class GnssReportProtoTest
         int compressedDataLength = compresser.deflate(output);
         compresser.end();
 
-        assertTrue(serialized.length < 260);
+//        assertTrue(serialized.length < 260);
 
         System.out.println(serialized.length);
+
+        byte[] bytesFromGoTenna = hexStringToByteArray(GoTennaSent);
+        try
+        {
+
+
+
+            GnssReportProto.GnssReport goTennaDeserialized = GnssReportProto.GnssReport.parseFrom(bytesFromGoTenna);
+
+
+            System.out.println(goTennaDeserialized);
+
+        } catch (InvalidProtocolBufferException e)
+        {
+            e.printStackTrace();
+        }
+
+
     }
+
+
+    private static String GoTennaSent = "0A1163616E6172795F74616E6F6F6B695F303112001DFCD1B444220B08EC82C1DD0510D0CCBF7D2A04080118032A060802100118032A06080110021801";
+
+
+
+
+
+    private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
+    public static String bytesToHexString(byte[] bytes)
+    {
+        if (bytes == null)
+        {
+            return "";
+        }
+
+        char[] hexChars = new char[bytes.length * 2];
+
+        for (int j = 0; j < bytes.length; j++)
+        {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+
+        return new String(hexChars);
+    }
+    public static byte[] hexStringToByteArray(String string)
+    {
+        int len = string.length();
+        byte[] data = new byte[len / 2];
+
+        for (int i = 0; i < len; i += 2)
+        {
+            data[i / 2] = (byte) ((Character.digit(string.charAt(i), 16) << 4) + Character.digit(string.charAt(i + 1), 16));
+        }
+
+        return data;
+    }
+
+
 }
